af631dc3a2c6f585c600512ce535185410c8e39e
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-06-04 16:27:48 +0300

    modify HW 3

--- seminar/03/hw/src/ru/gb/hw3/Program.java
+++ seminar/03/hw/src/ru/gb/hw3/Program.java
@@ -1,7 +1,18 @@
 package ru.gb.hw3;
 
+import ru.gb.hw3.modules.Worker;
+import ru.gb.hw3.modules.fixedPayWorker;
+import ru.gb.hw3.modules.hourlyPayWorker;
+
+import java.util.ArrayList;
+
 public class Program {
     public static void main(String[] args) {
-
+        ArrayList<Worker> workers = new ArrayList<>();
+        workers.add(new fixedPayWorker("Ivan", "Ivanov", 80000.0));
+        workers.add(new hourlyPayWorker("Petr", "Petrov", 500.0));
+        for (Worker worker : workers) {
+            System.out.println(worker);
+        }
     }
 }
